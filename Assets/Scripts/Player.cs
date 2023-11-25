7378cd826f6a903e833982648138e3c76437764c
Daniel Russom <daniel.russom00@gmail.com>
2023-11-25 01:32:50 +0000

    Serialize field for Player rigidBody

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,8 @@ public class Player : MonoBehaviour
     public float speed = 5;
     public float jumpForce = 10;
 
-    public Rigidbody2D body;
+    [SerializeField]
+    private Rigidbody2D body;
 
     void Update()
     {
@@ -14,6 +15,7 @@ public class Player : MonoBehaviour
         var xSpeed = Input.GetAxis("Horizontal") * speed;
         body.velocity = new Vector2(xSpeed, body.velocity.y); ;
 
+        //TODO No more air jumping qwq
         if (Input.GetButtonDown("Jump"))
         {
             body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
