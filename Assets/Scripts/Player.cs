3bc5af17852c7131c7e0c64cade11bb68205e182
Daniel Russom <daniel.russom00@gmail.com>
2023-12-02 11:01:58 +0000

    Jumping requires touching - but where's the refactoring?

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts.UnityEnums;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -5,20 +6,42 @@ public class Player : MonoBehaviour
     public float speed = 5;
     public float jumpForce = 10;
 
+    public float raycastLength = 0.1f;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isGrounded;
+
+    [SerializeField]
+    private Collider2D playerCollider;
+
     [SerializeField]
     private Rigidbody2D body;
 
     void Update()
     {
+        isGrounded = Physics2D.IsTouchingLayers(playerCollider, groundLayer);//.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
         //TODO Refactoring?
-        //TODO Delta? Investigate
-        var xSpeed = Input.GetAxis("Horizontal") * speed;
+        var xSpeed = Input.GetAxis(Inputs.Horizontal.ToString()) * speed;
         body.velocity = new Vector2(xSpeed, body.velocity.y); ;
 
         //TODO No more air jumping qwq
-        if (Input.GetButtonDown("Jump"))
+
+
+        if (Input.GetButtonDown(Inputs.Jump.ToString()) && isGrounded)
         {
-            body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+                body.AddForce(Vector3.up * 5, ForceMode2D.Impulse);
         }
     }
+    bool IsGrounded()
+    {
+        // Cast a ray straight down to check if the player is grounded
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, raycastLength);
+
+        // If the ray hits something, the player is grounded
+        return hit.collider != null;
+    }
+
 }
