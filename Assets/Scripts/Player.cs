e7106b0769030e8abd0ccd89e886bafcbbd913e7
Daniel Russom <daniel.russom00@gmail.com>
2023-12-02 11:05:44 +0000

    Clear up unused code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,7 +10,6 @@ public class Player : MonoBehaviour
     public Transform groundCheck;
     public float groundCheckRadius;
     public LayerMask groundLayer;
-    private bool isGrounded;
 
     [SerializeField]
     private Collider2D playerCollider;
@@ -20,28 +19,24 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        isGrounded = Physics2D.IsTouchingLayers(playerCollider, groundLayer);//.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-
         //TODO Refactoring?
         var xSpeed = Input.GetAxis(Inputs.Horizontal.ToString()) * speed;
-        body.velocity = new Vector2(xSpeed, body.velocity.y); ;
+        body.velocity = new Vector2(xSpeed, body.velocity.y);
 
-        //TODO No more air jumping qwq
-
-
-        if (Input.GetButtonDown(Inputs.Jump.ToString()) && isGrounded)
+        if (ShouldJump())
         {
-
-                body.AddForce(Vector3.up * 5, ForceMode2D.Impulse);
+            Jump();
         }
     }
-    bool IsGrounded()
-    {
-        // Cast a ray straight down to check if the player is grounded
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, raycastLength);
 
-        // If the ray hits something, the player is grounded
-        return hit.collider != null;
+    private bool ShouldJump()
+    {
+        var isGrounded = Physics2D.IsTouchingLayers(playerCollider, groundLayer);
+        return Input.GetButtonDown(Inputs.Jump.ToString()) && isGrounded;
     }
 
+    private void Jump()
+    {
+        body.AddForce(Vector3.up * 5, ForceMode2D.Impulse);
+    }
 }
