f4cca5b55d5727c0b917c3c91e0a51f6a8c71405
D. Azevedo <rplazevedo@gmail.com>
2023-12-02 15:34:43 +0000

    Fixed line renderer

--- Assets/Scripts/SwingingObject.cs
+++ Assets/Scripts/SwingingObject.cs
@@ -12,12 +12,10 @@ public class SwingingBlock : MonoBehaviour
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.startColor = Color.black;
         lineRenderer.endColor = Color.black;
-        lineRenderer.startWidth = 0.1f;
-        lineRenderer.endWidth = .1f;
+        lineRenderer.startWidth = 0.05f;
+        lineRenderer.endWidth = .05f;
         lineRenderer.positionCount = 2;
         lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
-        lineRenderer.material.color = Color.red;
-        lineRenderer.widthMultiplier = 0.2f;
         lineRenderer.useWorldSpace = true;
 
         distanceJoint = GetComponent<DistanceJoint2D>();
@@ -28,7 +26,8 @@ public class SwingingBlock : MonoBehaviour
         // TODO: make the rope connect the actual anchor point on the edge of the sprite
         var connectedAnchor = new Vector3 (distanceJoint.connectedAnchor.x, distanceJoint.connectedAnchor.y, 0);
         var anchor = new Vector3(distanceJoint.anchor.x, distanceJoint.anchor.y, 0);
-        DrawLine(transform.position, connectedAnchor);
+        var world_anchor = transform.TransformPoint(anchor);
+        DrawLine(world_anchor, connectedAnchor);
     }
     //For drawing line in the world space, provide the x,y,z values
 
