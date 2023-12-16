a657ee82cdbbe8075f262b89298f30b24dd5d84d
Daniel Russom <daniel.russom00@gmail.com>
2023-12-16 19:36:21 +0000

    Coin floating animation

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,8 +1,24 @@
 using Assets.Scripts.UnityEnums;
+using System;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    public float floatStrength = 0.5f;
+
+    private float originalYPosition;
+
+    void Start()
+    {
+        originalYPosition = transform.position.y;
+    }
+
+    private void Update()
+    {
+        var yOffset = (float) Math.Sin(Time.time) * floatStrength;
+        transform.position = new Vector2(transform.position.x, originalYPosition + yOffset);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag(Tags.Player))
