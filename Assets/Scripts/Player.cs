19fe083e4f54162af00b84c477c2d538f3c82257
D. Azevedo <rplazevedo@gmail.com>
2023-12-26 15:51:11 +0000

    Can not longer grapple coins

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -140,7 +140,7 @@ public class Player : MonoBehaviour
     {   
         var mouseCoord = cam.ScreenToWorldPoint(Input.mousePosition);
         RaycastHit2D hit = Physics2D.Linecast(transform.position, (mouseCoord - transform.position) * maxRange);
-        if (hit)
+        if (hit && hit.collider.gameObject.layer == 6)
         {
             distanceJoint.enabled = true;
             lineRenderer.enabled = true;
